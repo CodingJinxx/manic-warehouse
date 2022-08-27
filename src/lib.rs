784ecdf3fdc37477c21fed338a601dd24abaad6a
Jay Leimer <jay@jinxx.eu>
2022-08-27 19:19:21 +0200

    Added a red cube

--- src/lib.rs
+++ src/lib.rs
@@ -39,4 +39,19 @@ fn spawn_basic_scene(
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube{ size: 1.0})),
+        material: materials.add(Color::rgb(0.8, 0.1, 0.1).into()),
+        transform: Transform::from_xyz(0.0,0.5,0.0),
+        ..default()
+    });
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
 }
