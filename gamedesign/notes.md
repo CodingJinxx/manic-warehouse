d4c77b43b1ac282ab7e3c657db128d5034fc8176
Jay Leimer <jay@jinxx.eu>
2022-08-27 19:14:25 +0200

    Added basic scene

---
+++ gamedesign/notes.md
@@ -0,0 +1,9 @@
+# Manic Warehouse
+
+Roguelite where you need to earn money to pay rent
+
+Each time you run out of money to for rent you get sent back to your moms garage
+
+There you dont pay rent
+
+However you can only accept small contracts due to space restrictions
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,42 @@
 use bevy::prelude::*;
 
-pub const LAUNCHER_TITLE: &str = "Bevy Shell - Template";
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+pub const LAUNCHER_TITLE: &str = "Manic Warehouse";
 
 pub fn app() -> App {
     let mut app = App::new();
     app.insert_resource(WindowDescriptor {
+        width: WIDTH,
+        height: HEIGHT,
+        resizable: false,
         title: LAUNCHER_TITLE.to_string(),
         canvas: Some("#bevy".to_string()),
         fit_canvas_to_parent: true,
         ..Default::default()
     })
+    .insert_resource(ClearColor(Color::rgb(0.2,0.2,0.2)))
     .add_plugins(DefaultPlugins)
-    .add_startup_system(load_icon);
+    .add_startup_system(startup)
+    .add_startup_system(spawn_basic_scene);
     app
 }
 
-fn load_icon(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera2dBundle::default());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("bevy.png"),
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane{ size: 5.0})),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
 }
